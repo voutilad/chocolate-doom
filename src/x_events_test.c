6dbb9ebf2fe30a820cd376e4b3f812bb89838595
Dave Voutila <voutilad@gmail.com>
2020-03-02 21:19:03 -0500

    cJSON_free -> cJSON_Delete
    Turns out _free doesn't recursively free parts of a json object.
    
    This solves a leak found with valgrind.

--- src/x_events_test.c
+++ src/x_events_test.c
@@ -22,6 +22,14 @@
 int main()
 {
     mobj_t m1, m2;
+    m1.x = 10;
+    m1.y = 20;
+    m1.z = 0;
+    m1.type = MT_SHOTGUY;
+    m2.x = 11;
+    m2.y = 22;
+    m2.z = 1;
+    m2.type = MT_BARREL;
 
     if (X_InitLog(69, 69) != 0) {
         printf("failed to init log\n");
@@ -30,6 +38,11 @@ int main()
     X_LogStart(69, 69, 1);
     X_LogArmorPickup(69);
     X_LogWeaponPickup(wp_shotgun);
+
+    X_LogTargeted(&m1, &m2);
+    X_LogEnemyKilled(&m1);
+    X_LogMove(&m1);
+    X_LogPlayerDied(&m1);
     X_CloseLog();
     return 0;
 }
