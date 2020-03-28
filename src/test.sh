c2dbcd314e5e6f58f4296af1f387c7f07819165f
Dave Voutila <voutilad@gmail.com>
2020-03-28 16:16:55 -0400

    test.sh not working cross-platform
    simplify the pkg-config call...SDL2 is implied so it's not
    needed if including SDL2_net or SDL2_mixer. This resolves
    a cross-platform issue where on OpenBSD the library is
    named sdl2 and not SDL2

--- src/test.sh
+++ src/test.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 
-#cc -O0 -g -o test_events -DTEST -I doom/ -I . -I .. \
-#    -L/usr/local/lib -L/usr/X11R6/lib -lSDL2 \
-#    m_argv.o z_native.o cJSON.o i_system.o m_misc.o i_timer.o m_config.o tables.o \
-#    doom/x_events.c x_events_test.c
-
 cc -O0 -g -o test_events -DTEST -DX_LOG_MOVEMENT -I doom/ -I . -I .. \
-    $(pkg-config --cflags --libs SDL2 SDL2_net SDL2_mixer) \
+    $(pkg-config --cflags --libs SDL2_net SDL2_mixer) \
     m_argv.c z_native.c cJSON.c i_system.c m_misc.c i_timer.c m_config.c tables.c \
     doom/x_events.c x_events_test.c
