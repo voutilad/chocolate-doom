458292a105a06b76ae795882c762c5bcfeb3387d
Dave Voutila <voutilad@gmail.com>
2020-03-30 11:10:03 -0400

    Flycheck not picking up local headers, etc.
    Updates the .dir-locals.el file to dynamically expand the flycheck gcc
    and clang include paths for headers in the chocolate-doom project.
    
    NOTE: This means you MUST HAVE PROJECTILE AND FLYCHECK MODES!!!

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,24 @@
+;; NOTE: This assumes you have flycheck and projectile!
 ((c-mode . ((c-file-style . "bsd")
              (indent-tabs-mode . nil)
              (tab-width . 8)
-             (c-basic-offset . 4))))
+             (c-basic-offset . 4)
+             (eval . (let* ((paths '(""
+                                     "midiproc"
+                                     "opl"
+                                     "pcsound"
+                                     "textscreen"
+                                     "src"
+                                     "src/doom"
+                                     "src/heretic"
+                                     "src/hexen"
+                                     "src/strife"
+                                     "src/setup"))
+                            (full-paths (mapcar
+                                         (lambda (path)
+                                           (expand-file-name path (projectile-project-root)))
+                                         paths)))
+                       (setq-local flycheck-gcc-include-path
+                             (append flycheck-gcc-include-path full-paths))
+                       (setq-local flycheck-clang-include-path
+                             (append flycheck-clang-include-path full-paths)))))))
