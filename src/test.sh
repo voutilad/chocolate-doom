c0f9e502a50ccb1ce110daef123b7edf74076cae
Dave Voutila <voutilad@gmail.com>
2020-04-01 06:29:35 -0400

    Fix test builds on ubuntu 18.04/gcc7
    GCC 7.x seems to be very picky about the order of arguments.
    
    In this case, we need the libraries to come at the end,
    otherwise the linker freaks out and can't find SDL stuff.

--- src/test.sh
+++ src/test.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-cc -O0 -g -o test_events -DTEST -DX_LOG_MOVEMENT -I doom/ -I . -I .. \
-    $(pkg-config --cflags --libs SDL2_net SDL2_mixer) \
+cc -O0 -g -o test_events -DTEST -I doom/ -I . -I .. \
+    $(pkg-config --cflags SDL2_net SDL2_mixer) \
     m_argv.c z_native.c cJSON.c i_system.c m_misc.c i_timer.c m_config.c tables.c \
-    doom/x_events.c x_events_test.c
+    doom/x_events.c x_events_test.c \
+    $(pkg-config --libs SDL2_net SDL2_mixer)
